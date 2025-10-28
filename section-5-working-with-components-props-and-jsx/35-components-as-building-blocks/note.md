85abb280d1fd36b0247ea366114fa61ef1b5930e
Mohammad Faqusa <103820177+mohammad-faqusa@users.noreply.github.com>
2025-10-28 10:39:56 +0200

    35. Components as Building Blocks

---
+++ section-5-working-with-components-props-and-jsx/35-components-as-building-blocks/note.md
@@ -0,0 +1,10 @@
+## 35. Components as Building Blocks
+
+#### what are react componenets
+
+- building blocks of user interface
+- react render each block to make UI
+- componenet has
+  - data , logic , and appearance
+- nested , resued components
+- ![alt text](image.png)
--- titleConverter.py
+++ titleConverter.py
@@ -106,7 +106,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 5: Working With Components, Props, and JSX"
 # section_title = "coding-exercises"
 
-part_title = "34. Before We Start Coding: Debugging"
+part_title = "35. Components as Building Blocks"
 print(f"export PART_TITLE='{part_title}'")
 
 prepare_resources(section_title, part_title)
