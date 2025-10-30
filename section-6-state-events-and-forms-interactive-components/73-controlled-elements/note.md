1e2d0b2c0dbfe2b320debab4a135e9d2e8b7b531
Mohammad Faqusa <103820177+mohammad-faqusa@users.noreply.github.com>
2025-10-30 08:29:04 +0200

    73. Controlled Elements

---
+++ section-6-state-events-and-forms-interactive-components/73-controlled-elements/note.md
@@ -0,0 +1,3 @@
+## 73. Controlled Elements
+
+#### hwo to use contolled elements
--- travellist/src/App.js
+++ travellist/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index.css";
 
 const initialItems = [
@@ -21,22 +22,43 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [selection, setSelection] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+
+    const newItem = { description, selection, packed: false, id: Date.now() };
+
+    console.log(newItem);
+
+    setDescription("");
+    setSelection(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your trip ?</h3>
-      <select>
+      <select
+        value={selection}
+        onChange={(e) => {
+          setSelection(Number(e.target.value));
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
