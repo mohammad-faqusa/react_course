37940213a9c693babc636790e903ad8500f79015
Mohammad Faqusa <103820177+mohammad-faqusa@users.noreply.github.com>
2025-10-31 17:43:25 +0200

    82. Deleting an Item: More Child-to-Parent Communication!

---
+++ section-6-state-events-and-forms-interactive-components/82-deleting-an-item-more-child-to-parent-communication/note.md
@@ -0,0 +1,10 @@
+## 82. Deleting an Item: More Child-to-Parent Communication!
+
+#### the click to delete example
+
+1. create handleDeleteItem in App
+   1. call setItems
+2. pass the function to packagelist
+3. pass the function to each item
+4. onClick button
+5.
--- travellist/src/App.js
+++ travellist/src/App.js
@@ -13,11 +13,15 @@ export default function App() {
     setItems((l) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((l) => l.filter((e) => e.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItem={handleAddItem} />
-      <PackageList items={items} />
+      <PackageList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -71,24 +75,25 @@ function Form({ onAddItem }) {
   );
 }
 
-function PackageList({ items }) {
+function PackageList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.description}
       </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
